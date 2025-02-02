b882605a43fae05220ec66ed564d4377898008d2
Maks Vakulenko <maksimvakulenkooo@gmail.com>
2025-02-02 17:00:12 +0100

    feat: topic, Newspaper, Redactor models added and created migrations for models

--- newspaper/admin.py
+++ newspaper/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from newspaper.models import Redactor
+
+admin.site.register(Redactor)
--- newspaper/models.py
+++ newspaper/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.PositiveIntegerField()
+
+    class Meta:
+        verbose_name = "Redactor"
+        verbose_name_plural = "Redactors"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    pub_date = models.DateTimeField()
+    topic = models.ManyToManyField(Topic, related_name="newspapers")
+    publishers = models.ManyToManyField(Redactor, related_name="newspapers")
+
+    def __str__(self):
+        return self.title
